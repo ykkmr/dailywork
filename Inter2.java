5025e6acdb81d8bb97bc4cf336134abc71075ba8
ykkmr <65176715+ykkmr@users.noreply.github.com>
2021-03-16 09:26:55 +0900

    practice lambda

---
+++ Inter2.java
@@ -0,0 +1,12 @@
+package day0315;
+
+/**
+ *	functionalInterface annotation을 정의하면 abstract method를 하나만 정의할 수 있다
+ * @author user
+ */
+ @FunctionalInterface
+public interface Inter2 {
+	//반환형없고 매개변수 있는형
+	public abstract void test(String name); //abstract method
+	
+}//interface
