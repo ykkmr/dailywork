0ffc374574af27a3291a419319813a54b55523e3
ykkmr <65176715+ykkmr@users.noreply.github.com>
2021-06-16 16:23:50 +0900

    Add files via upload

---
+++ EmpDomain.java
@@ -0,0 +1,39 @@
+package kr.co.sist.domain;
+
+public class EmpDomain {
+	private int empno,sal ;
+	private String  ename, job, hiredate;
+	
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getHiredate() {
+		return hiredate;
+	}
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
+	
+	
+}
