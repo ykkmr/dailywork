e89801e6be673c9f523877df3904ac13583d75d3
ykkmr <65176715+ykkmr@users.noreply.github.com>
2021-06-18 23:02:59 +0900

    Add files via upload

---
+++ DeptDomain.java
@@ -0,0 +1,21 @@
+package kr.co.sist.domain;
+
+public class DeptDomain {
+	private int deptno;
+	private String dname;
+	
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	
+	
+}
