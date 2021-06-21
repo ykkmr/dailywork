b637b25a22eac64ab6eea166fd885188a663f8cc
ykkmr <65176715+ykkmr@users.noreply.github.com>
2021-06-21 17:41:31 +0900

    Add files via upload

---
+++ TableVO.java
@@ -0,0 +1,14 @@
+package kr.co.sist.vo;
+
+public class TableVO {
+	private String loc;
+
+	public String getLoc() {
+		return loc;
+	}
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+
+}
