06b80a7f402807a4da8985e424e1ad9baee767e7
ykkmr <65176715+ykkmr@users.noreply.github.com>
2021-06-21 17:41:16 +0900

    Add files via upload

---
+++ BoardDomain.java
@@ -0,0 +1,40 @@
+package kr.co.sist.domain;
+
+public class BoardDomain {
+	private String model, car_year, car_option, hiredate;
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getCar_year() {
+		return car_year;
+	}
+
+	public void setCar_year(String car_year) {
+		this.car_year = car_year;
+	}
+
+	public String getCar_option() {
+		return car_option;
+	}
+
+	public void setCar_option(String car_option) {
+		this.car_option = car_option;
+	}
+
+	public String getHiredate() {
+		return hiredate;
+	}
+
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
+	
+	
+	
+}
