adff2a0b3de91c075678ecdcd11f0aa1245ee2e3
ykkmr <65176715+ykkmr@users.noreply.github.com>
2021-06-21 17:34:53 +0900

    Add files via upload

---
+++ JoinDomain.java
@@ -0,0 +1,57 @@
+package kr.co.sist.domain;
+
+public class JoinDomain {
+private int deptno, empno, sal;
+private String dname, loc, ename, job, hiredate;
+
+public int getDeptno() {
+	return deptno;
+}
+public void setDeptno(int deptno) {
+	this.deptno = deptno;
+}
+public int getEmpno() {
+	return empno;
+}
+public void setEmpno(int empno) {
+	this.empno = empno;
+}
+public int getSal() {
+	return sal;
+}
+public void setSal(int sal) {
+	this.sal = sal;
+}
+public String getDname() {
+	return dname;
+}
+public void setDname(String dname) {
+	this.dname = dname;
+}
+public String getLoc() {
+	return loc;
+}
+public void setLoc(String loc) {
+	this.loc = loc;
+}
+public String getEname() {
+	return ename;
+}
+public void setEname(String ename) {
+	this.ename = ename;
+}
+public String getJob() {
+	return job;
+}
+public void setJob(String job) {
+	this.job = job;
+}
+public String getHiredate() {
+	return hiredate;
+}
+public void setHiredate(String hiredate) {
+	this.hiredate = hiredate;
+}
+
+
+}
