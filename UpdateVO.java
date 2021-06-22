571a9433917c0fdd1efcfdfc4241d5a9deb51b35
ykkmr <65176715+ykkmr@users.noreply.github.com>
2021-06-22 19:07:09 +0900

    Add files via upload

---
+++ UpdateVO.java
@@ -0,0 +1,33 @@
+package kr.co.sist.vo;
+
+public class UpdateVO {
+	private int deptno, empno; // 0
+	private String ename, job; // null
+	
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public int getEmpno() {
+		return empno;
+	}
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+	
+}
