aba5162e2569c56c61c13e348ee1ce8217f337e4
ykkmr <65176715+ykkmr@users.noreply.github.com>
2021-06-22 19:07:20 +0900

    Add files via upload

---
+++ DeptVO.java
@@ -0,0 +1,15 @@
+package kr.co.sist.vo;
+
+public class DeptVO {
+	private int deptno;
+
+	public int getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	
+	
+}//class
