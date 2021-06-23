8ff093cc7b7abb80b30ae0603a1f99c118e6106e
ykkmr <65176715+ykkmr@users.noreply.github.com>
2021-06-23 15:43:47 +0900

    Add files via upload

---
+++ ZipcodeDomain.java
@@ -0,0 +1,47 @@
+package kr.co.sist.domain;
+
+public class ZipcodeDomain {
+	private String zipcode, sido, gugun, dong, bunji;
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getSido() {
+		return sido;
+	}
+
+	public void setSido(String sido) {
+		this.sido = sido;
+	}
+
+	public String getGugun() {
+		return gugun;
+	}
+
+	public void setGugun(String gugun) {
+		this.gugun = gugun;
+	}
+
+	public String getDong() {
+		return dong;
+	}
+
+	public void setDong(String dong) {
+		this.dong = dong;
+	}
+
+	public String getBunji() {
+		return bunji;
+	}
+
+	public void setBunji(String bunji) {
+		this.bunji = bunji;
+	}
+	
+	
+}
